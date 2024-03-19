1f51a632862f85f1b5869e5b34919826a6b92c23
twoandtwelve <jackykim573@gmail.com>
2024-03-19 21:19:37 +1300

    added projects and style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,49 @@
 import React from "react"
+import './Projects.css'
+import harderturrets from '../images/harderturrets.png'
 
 export default function Projects() {
     return (
-        <section id='projects'>Projects here</section>
+        <section id='projects'>
+            <h2 className='title'>Projects</h2>
+            <div className='projects_container'>
+                <article className='article'>
+                    <div>
+                        <img className='project_image' src={harderturrets}/>
+                    </div>
+                    <h3 className='project_title'>HarderTurrets</h3>
+                    <p className='project_description'>A Lethal Company mod</p>
+                    <a className='project_button' href='https://thunderstore.io/c/lethal-company/p/jacky/HarderTurrets/'>Link</a>
+                    <a className='project_button' href='https://github.com/twoandtwelve/HarderTurrets'>Github</a>
+                </article>
+                <article className='article'>
+                    <div>
+                        <img className='project_image' src={harderturrets}/>
+                    </div>
+                    <h3 className='project_title'>HarderTurrets</h3>
+                    <p className='project_description'>A Lethal Company mod</p>
+                    <a className='project_button' href='https://thunderstore.io/c/lethal-company/p/jacky/HarderTurrets/'>Link</a>
+                    <a className='project_button' href='https://github.com/twoandtwelve/HarderTurrets'>Github</a>
+                </article>
+                <article className='article'>
+                    <div>
+                        <img className='project_image' src={harderturrets}/>
+                    </div>
+                    <h3 className='project_title'>HarderTurrets</h3>
+                    <p className='project_description'>A Lethal Company mod</p>
+                    <a className='project_button' href='https://thunderstore.io/c/lethal-company/p/jacky/HarderTurrets/'>Link</a>
+                    <a className='project_button' href='https://github.com/twoandtwelve/HarderTurrets'>Github</a>
+                </article>
+                <article className='article'>
+                    <div>
+                        <img className='project_image' src={harderturrets}/>
+                    </div>
+                    <h3 className='project_title'>HarderTurrets</h3>
+                    <p className='project_description'>A Lethal Company mod</p>
+                    <a className='project_button' href='https://thunderstore.io/c/lethal-company/p/jacky/HarderTurrets/'>Link</a>
+                    <a className='project_button' href='https://github.com/twoandtwelve/HarderTurrets'>Github</a>
+                </article>
+            </div>
+        </section>
     )   
 }
