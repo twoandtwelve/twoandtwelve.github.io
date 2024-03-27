e153cadbaefaf945283d2c74cc2994f9111c02fc
twoandtwelve <jackykim573@gmail.com>
2024-03-28 02:56:33 +1300

    rearranged projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,15 +16,15 @@ export default function Projects() {
         <section id='projects' className='max-w-3xl mx-auto px-8 py-6'>
             <h2 className='mb-10 font-medium text-xl'>projects</h2>
             <div className='flex flex-wrap justify-center gap-4'>
-                <article className='flex flex-col max-w-screen-sm sm:max-w-project border border-slate-500 rounded-md bg-white drop-shadow-lg'>   
-                    <img src={Harder_Turrets} className='rounded-t-md'/>
+            <article className='flex flex-col max-w-screen-sm sm:max-w-project border border-slate-500 rounded-md bg-white drop-shadow-lg'>
+                    <img src={Infinite_Spray_Cans} className='rounded-t-md'/>
                     <div className='p-4 pb-0 grow'>
-                        <h3 className='font-semibold text-l'>harder turrets</h3>    
-                        <p>A Lethal Company mod that increases the spawnrate & difficulty of turrets.</p>
+                        <h3 className='font-semibold text-l'>infinite spray cans</h3>
+                        <p>A Lethal Company mod that allows infinite use of the spray paint can item.</p>
                     </div>
                     <div className='p-4 flex justify-end items-center gap-4 text-2xl'>
-                        <a href='https://thunderstore.io/c/lethal-company/p/jacky/HarderTurrets/' target='_blank'><GoLinkExternal /></a>
-                        <a href='https://github.com/twoandtwelve/HarderTurrets' target='_blank'><AiOutlineGithub /></a>
+                        <a href='https://thunderstore.io/c/lethal-company/p/jacky/InfiniteSprayCans/' target='_blank'><GoLinkExternal /></a>
+                        <a href='https://github.com/twoandtwelve/InfiniteSprayCans' target='_blank'><AiOutlineGithub /></a>
                     </div>
                 </article>
                 <article className='flex flex-col max-w-screen-sm sm:max-w-project border border-slate-500 rounded-md bg-white drop-shadow-lg'>
@@ -38,15 +38,15 @@ export default function Projects() {
                     <a href='https://github.com/twoandtwelve/twoandtwelve.github.io' target='_blank'><AiOutlineGithub /></a>
                     </div>
                 </article>
-                <article className='flex flex-col max-w-screen-sm sm:max-w-project border border-slate-500 rounded-md bg-white drop-shadow-lg'>
-                    <img src={Infinite_Spray_Cans} className='rounded-t-md'/>
+                <article className='flex flex-col max-w-screen-sm sm:max-w-project border border-slate-500 rounded-md bg-white drop-shadow-lg'>   
+                    <img src={Harder_Turrets} className='rounded-t-md'/>
                     <div className='p-4 pb-0 grow'>
-                        <h3 className='font-semibold text-l'>infinite spray cans</h3>
-                        <p>A Lethal Company mod that allows infinite use of the spray paint can item.</p>
+                        <h3 className='font-semibold text-l'>harder turrets</h3>    
+                        <p>A Lethal Company mod that increases the spawnrate & difficulty of turrets.</p>
                     </div>
                     <div className='p-4 flex justify-end items-center gap-4 text-2xl'>
-                        <a href='https://thunderstore.io/c/lethal-company/p/jacky/InfiniteSprayCans/' target='_blank'><GoLinkExternal /></a>
-                        <a href='https://github.com/twoandtwelve/InfiniteSprayCans' target='_blank'><AiOutlineGithub /></a>
+                        <a href='https://thunderstore.io/c/lethal-company/p/jacky/HarderTurrets/' target='_blank'><GoLinkExternal /></a>
+                        <a href='https://github.com/twoandtwelve/HarderTurrets' target='_blank'><AiOutlineGithub /></a>
                     </div>
                 </article>
                 <article className='flex flex-col max-w-screen-sm sm:max-w-project border border-slate-500 rounded-md bg-white drop-shadow-lg'>
