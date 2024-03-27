dc1e3a25dcc0a74949c424fc32e7ce0137e0c1f8
twoandtwelve <jackykim573@gmail.com>
2024-03-28 03:50:24 +1300

    added burger menu icon for mobile screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,30 @@
-import React from "react"
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { GoDownload } from "react-icons/go";
+import { CiMenuBurger } from "react-icons/ci";
 import CV from "/public/files/jackykim_cv.pdf"
 
 
 export default function Nav() {
+
+    const [open, setOpen] = useState(false);
+
     return (
         <nav id='home' className='max-w-3xl mx-auto px-8 py-16'>
-            <div className='flex flex-col gap-2 sm:flex-row sm:gap-0 sm:justify-between sm:items-center'>
+            <div className='flex flex-row gap-2 justify-between items-center'>
                 <div className="flex gap-4">
                     <Link to='home' smooth duration={500} className='cursor-pointer'>home</Link>
                     <Link to='projects' smooth duration={500} className='cursor-pointer'>projects</Link>
                     <Link to='about' smooth duration={500} className='cursor-pointer'>about me</Link>
                 </div>
-                <div className="flex gap-4">
+                <div className="gap-4 hidden sm:flex">
                     <a href={CV} download="jackykim_cv.pdf" target="_blank" className ='flex items-center gap-1'>resume<GoDownload/></a>
                     <a href='https://github.com/twoandtwelve' target="_blank" >github</a>
                     <a href='https://www.linkedin.com/in/jacky-kim-21b154263/' target="_blank">linkedIn</a>
                 </div>
+                <button className="flex sm:hidden" onClick={() => setOpen(!open)}>
+                    <CiMenuBurger />
+                </button>
             </div>
         </nav>
     )   
