048b13d3983949f00fb26a8da61fa07a1b5793c4
twoandtwelve <jackykim573@gmail.com>
2024-03-30 23:14:08 +1300

    added text to tech stack images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,32 +15,35 @@ export default function About() {
                 <div className='flex flex-col'>
                     <div className='flex gap-2 flex-col sm:flex-row mb-6'>
                         <div className='flex gap-2'>
-                            <div className='flex justify-center items-center aspect-square w-24 text-5xl text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
-                                <p><FaJava/></p>
+                            <div className='flex flex-col gap-1 justify-center items-center aspect-square w-24 text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
+                                <p className='text-4xl'><FaJava/></p>
+                                <p className='text-base'>Java</p>
                             </div>
-                            <div className='flex justify-center items-center aspect-square w-24 text-5xl text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
-                                <p><TbBrandCSharp/></p>
+                            <div className='flex flex-col gap-1 justify-center items-center aspect-square w-24 text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
+                                <p className='text-4xl'><FaPython/></p>
+                                <p className='text-base'>Python</p>
                             </div>
-                            <div className='flex justify-center items-center aspect-square w-24 text-5xl text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
-                                <p><FaPython/></p>
+                            <div className='flex justify-center items-center aspect-square w-24 text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
+                                <p className='text-5xl'><TbBrandCSharp/></p>
                             </div>
                         </div>
                         <div className='flex gap-2'>
-                            <div className='flex justify-center items-center aspect-square w-24 text-5xl text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
-                                <p><FaHtml5/></p>
+                            <div className='flex flex-col gap-1 justify-center items-center aspect-square w-24 text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
+                                <p className='text-4xl'><FaHtml5/></p>
+                                <p className='text-base'>HTML</p>
                             </div>
-                            <div className='flex justify-center items-center aspect-square w-24 text-5xl text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
-                                <p><FaCss3Alt/></p>
+                            <div className='flex flex-col gap-1 justify-center items-center aspect-square w-24 text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
+                                <p className='text-4xl'><FaCss3Alt/></p>
+                                <p className='text-base'>CSS</p>
                             </div>
-                            <div className='flex justify-center items-center aspect-square w-24 text-5xl text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
-                                <p><IoLogoJavascript/></p>
+                            <div className='flex flex-col gap-1 justify-center items-center aspect-square w-24 text-white border border-slate-700 bg-slate-700 hover:bg-white hover:text-slate-800 hover:border hover:border-slate-700 ease-in-out duration-200'>
+                                <p className='text-4xl'><IoLogoJavascript/></p>
+                                <p className='text-base'>JavaScript</p>
                             </div>
                         </div>
                     </div>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quasi
-                        hic dolorem eius vel nobis aspernatur dicta ratione! Cum eum dolorem aliquam
-                        fugit quibusdam, harum fuga quasi. Enim, obcaecati perspiciatis.
+                        
                     </p>
                 </div>
             </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,7 +22,7 @@ export default function Nav() {
                     <a href='https://github.com/twoandtwelve' target="_blank" >github</a>
                     <a href='https://www.linkedin.com/in/jacky-kim-21b154263/' target="_blank">linkedIn</a>
                 </div>
-                <div className='flex flex-col sm:hidden'>
+                <div className='flex flex-col sm:hidden'> 
                     <button className='flex justify-end mt-1' onClick={() => setOpen(!open)}>
                         <CiMenuBurger className='text-lg'/>
                     </button>
