e34965548da351afcfe98feb0898c040721c49c0
twoandtwelve <jackykim573@gmail.com>
2024-04-01 04:36:46 +1300

    edited tech stack text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,8 +49,8 @@ export default function About() {
                         <br/>
                         <br/>
                         Outside of university, I like to use <strong>Python</strong> and <strong>C#</strong> to build personal projects.
-                        I have a particular interest in reverse engineering and am familiar with decompiler software such as <strong>ILSpy</strong>, hence my passion for video game mod development. 
-                        For Front-End development, I enjoy using <strong>React</strong> and <strong>Tailwind CSS</strong> to streamline the coding process. 
+                        I have an interest in reverse engineering and am familiar with decompiler software such as <strong>ILSpy</strong>, hence my passion for video game mod development. 
+                        For Front-End development, I enjoy using <strong>React</strong> and <strong>Tailwind CSS</strong>.
                         <br />
                         <br />
                         <br />
