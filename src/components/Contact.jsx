76345f4d4bf863f82f3f753f3abe7ae5f87c6ff1
twoandtwelve <jackykim573@gmail.com>
2024-04-02 17:58:30 +1300

    hid contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react"
 export default function Contact() {
     return (
         <section id='contact' className='max-w-3xl mx-auto px-8 py-6'>
-            hi
+            <h2 className='mb-8 font-medium text-xl'>contact</h2>
         </section>
     )   
 }   
