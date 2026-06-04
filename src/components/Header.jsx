26a0fec71d414a0f88dddc95eb1e5f084a9071d4
jacky <jackykim573@gmail.com>
2026-06-04 21:09:18 +1200

    updated yml and header info

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
-import React from "react"
-
+import React from "react";
 
 export default function Header() {
-    return (
-        <header className='flex max-w-3xl mx-auto px-8 p-32'>
-                <div>
-                    <h1 className='mb-5 font-medium text-2xl'>hi, I'm jacky</h1>
-                    <p>I'm a penultimate year <strong>Software Engineering student @ UoA, NZ</strong>. 
-                        <br />
-                        I enjoy learning new technologies & working on projects that align with my personal interests. 
-                        <br />
-                        <br />
-                        Currently looking for <strong>2024/2025</strong> internship opportunities.
-                    </p>
-                </div> 
-        </header>
-    )   
+  return (
+    <header className="flex max-w-3xl mx-auto px-8 p-32">
+      <div>
+        <h1 className="mb-5 font-medium text-2xl">hi, I'm jacky</h1>
+        <p>
+          I'm a <strong>Software Engineering graduate @ UoA, NZ</strong>.
+          <br />
+          I enjoy learning new technologies & working on projects that align
+          with my personal interests.
+          <br />
+          <br />
+          Currently looking for <strong>2026</strong> graduate roles.
+        </p>
+      </div>
+    </header>
+  );
 }
